0f087406705f84a84214bd962c1b8dce0c54bb22
Oscar <oscardani.dev@outlook.com>
2023-08-03 15:26:18 -0600

    Fetch reduciendo promise a una linea

--- service/client-service.js
+++ service/client-service.js
@@ -45,26 +45,8 @@ PUT / PATCH
 DELETE
 */
 
-const listaClientes = () => {
-  const promise = new Promise( (resolve,reject) =>
-  {
-    const http = new XMLHttpRequest();
-    http.open("GET","http://localhost:3000/perfil");
-    
-    http.send();
-    
-    /* para que nos de respuesta */
-    http.onload = () => {
-        const response = JSON.parse(http.response);
-        if(http.status>= 400){
-          reject(response);
-        }else{
-          resolve(response);
-        }
-    };
-  });
-  return promise;
-}
+/* FETCH API */
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 listaClientes().then((data) => {
   data.forEach((perfil) => {
@@ -73,19 +55,3 @@ listaClientes().then((data) => {
   });
 }).catch((error)=>alert("ocurrio un error"));
 
-
-
-
-
-/* 
-    Muestra la fecha de hoy es solo muestra
-   const http2 = new XMLHttpRequest();
-   http2.open("GET","http://localhost:3000/perfil/hoy");
-   http2.send();
-   http2.onload = () => {
-    const data2 = JSON.parse(http2.response);
-   } */
-
-/* 
-visualiza las acciones 
-console.log(http) */
