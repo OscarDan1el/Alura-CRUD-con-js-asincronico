1c2e5b1f855c11e5de8e5df45c999b62024ca67f
Oscar <oscardani.dev@outlook.com>
2023-08-09 18:06:59 -0600

    try catch con asyc finalizando curso

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -15,10 +15,21 @@ const obtenerInformacion = async () => {
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
 
-    const perfil = await clientServices.detalleCliente(id) 
-            nombre.value = perfil.nombre;
-            email.value = perfil.email;    
+    /* cuando exista un error */
+    try {
+        const perfil = await clientServices.detalleCliente(id);
+        if(perfil.nombre && perfil.email){
+        nombre.value = perfil.nombre;
+        email.value = perfil.email;
+        }else{
+            throw new Error();
+        }  
      
+    } catch (error) {
+        window.location.href="/screens/error.html";
+    }
+
+    
 }
 obtenerInformacion();
 
